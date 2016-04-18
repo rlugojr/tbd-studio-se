689067d453383d6eb708fe22d6b7c5c03ba87507
Ismaël Mejía <imejia@talend.com>
2016-04-18 16:31:02 +0200

    upgrade atlas-client dependency to 0.7-incubating-SNAPSHOT
    and some small refactoring

--- main/components_lib/talend-cloudera-navigator-api/src/main/java/org/talend/governance/AtlasMapper.java
+++ main/components_lib/talend-cloudera-navigator-api/src/main/java/org/talend/governance/AtlasMapper.java
@@ -3,6 +3,7 @@ package org.talend.governance;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import org.apache.atlas.AtlasClient;
+import org.apache.atlas.AtlasServiceException;
 import org.apache.atlas.typesystem.Referenceable;
 import org.apache.atlas.typesystem.TypesDef;
 import org.apache.atlas.typesystem.json.InstanceSerialization;
@@ -11,7 +12,6 @@ import org.apache.atlas.typesystem.types.*;
 import org.apache.atlas.typesystem.types.utils.TypesUtil;
 import org.codehaus.jettison.json.JSONArray;
 import org.talend.cloudera.navigator.api.NavigatorNode;
-import scala.collection.JavaConversions;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -19,10 +19,15 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by ismael on 4/15/16.
+ * Class to map the Talend DAG into an Atlas DAG
  */
 public class AtlasMapper {
 
+    /**
+     * TODO this function must return the appropriate type of the objects that are going to be persisted
+     * @param navigatorNodes
+     * @param jobId
+     */
     public void map(List<NavigatorNode> navigatorNodes, String jobId) {
         // First create the type definition
         TypesDef typesDef = createTypeDefinitions();
@@ -32,110 +37,59 @@ public class AtlasMapper {
         // then we create the Entities
         Collection<Referenceable> refs = new ArrayList<Referenceable>();
         for (NavigatorNode navigatorNode : navigatorNodes) {
-            String name = navigatorNode.getName();
-            Map<String, String> schema = navigatorNode.getSchema();
-            List<String> inputNodes = navigatorNode.getInputNodes();
-            List<String> outputNodes = navigatorNode.getOutputNodes();
-
-            Referenceable referenceable = new Referenceable(name, "TODO");
-            for(Map.Entry<String, String> schem : schema.entrySet()) {
-                referenceable.set(schem.getKey(), schem.getValue());
-            }
-            referenceable.set("inputs", inputNodes);
-            referenceable.set("outputs", outputNodes);
-
-            refs.add(referenceable);
-            String entityJSON = InstanceSerialization.toJson(referenceable, true);
+            Referenceable ref = createEntity(navigatorNode);
+            refs.add(ref);
+            String entityJSON = InstanceSerialization.toJson(ref, true);
             System.out.println(entityJSON);
         }
+
 //        AtlasClient client = null;
-//        JSONArray guids = client.createEntity(refs);
+//        try {
+//            JSONArray guids = client.createEntity(refs);
+//        } catch (AtlasServiceException e) {
+//            e.printStackTrace();
+//        }
 
 //        metadataServiceClient.createType(typesAsJSON);
 //        // verify types created
 //        verifyTypesCreated();
     }
 
-    TypesDef createTypeDefinitions() {
-        List<EnumTypeDefinition> enums = ImmutableList.<EnumTypeDefinition>of();
-        List<StructTypeDefinition> structs = ImmutableList.<StructTypeDefinition>of();
+    /**
+     *
+     * @return
+     */
+    private TypesDef createTypeDefinitions() {
+        ImmutableList<EnumTypeDefinition> enums = ImmutableList.<EnumTypeDefinition>of();
+        ImmutableList<StructTypeDefinition> structs = ImmutableList.<StructTypeDefinition>of();
 
         HierarchicalTypeDefinition<TraitType> logTraitDef = TypesUtil.createTraitTypeDef("Log Data", null); //"LogData Trait",  null);
-        List<HierarchicalTypeDefinition<TraitType>> traits = ImmutableList.<HierarchicalTypeDefinition<TraitType>>of(logTraitDef);
+        ImmutableList<HierarchicalTypeDefinition<TraitType>> traits = ImmutableList.<HierarchicalTypeDefinition<TraitType>>of(logTraitDef);
 
-        List<HierarchicalTypeDefinition<ClassType>> classes = ImmutableList.<HierarchicalTypeDefinition<ClassType>>of();
+        ImmutableList<HierarchicalTypeDefinition<ClassType>> classes = ImmutableList.<HierarchicalTypeDefinition<ClassType>>of();
 
-//        return TypesUtil.getTypesDef(ImmutableList.<EnumTypeDefinition>of(), ImmutableList.<StructTypeDefinition>of(),
-//                ImmutableList.of(), ImmutableList.of());
-        return new TypesDef(JavaConversions.asScalaBuffer(enums), JavaConversions.asScalaBuffer(structs),
-                    JavaConversions.asScalaBuffer(traits), JavaConversions.asScalaBuffer(classes));
+        return TypesUtil.getTypesDef(ImmutableList.<EnumTypeDefinition>of(), ImmutableList.<StructTypeDefinition>of(),
+            traits, classes);
     }
 
-//    TypesDef createTypeDefinitions() throws Exception {
-//        HierarchicalTypeDefinition<ClassType> dbClsDef = TypesUtil
-//                .createClassTypeDef(DATABASE_TYPE, DATABASE_TYPE, null,
-//                        TypesUtil.createUniqueRequiredAttrDef("name", DataTypes.STRING_TYPE),
-//                        attrDef("description", DataTypes.STRING_TYPE), attrDef("locationUri", DataTypes.STRING_TYPE),
-//                        attrDef("owner", DataTypes.STRING_TYPE), attrDef("createTime", DataTypes.LONG_TYPE));
-//
-//        HierarchicalTypeDefinition<ClassType> storageDescClsDef = TypesUtil
-//                .createClassTypeDef(STORAGE_DESC_TYPE, STORAGE_DESC_TYPE, null, attrDef("location", DataTypes.STRING_TYPE),
-//                        attrDef("inputFormat", DataTypes.STRING_TYPE), attrDef("outputFormat", DataTypes.STRING_TYPE),
-//                        attrDef("compressed", DataTypes.STRING_TYPE, Multiplicity.REQUIRED, false, null));
-//
-//        HierarchicalTypeDefinition<ClassType> columnClsDef = TypesUtil
-//                .createClassTypeDef(COLUMN_TYPE, COLUMN_TYPE, null, attrDef("name", DataTypes.STRING_TYPE),
-//                        attrDef("dataType", DataTypes.STRING_TYPE), attrDef("comment", DataTypes.STRING_TYPE));
-//
-//        HierarchicalTypeDefinition<ClassType> tblClsDef = TypesUtil
-//                .createClassTypeDef(TABLE_TYPE, TABLE_TYPE, ImmutableSet.of("DataSet"),
-//                        new AttributeDefinition(DB_ATTRIBUTE, DATABASE_TYPE, Multiplicity.REQUIRED, false, null),
-//                        new AttributeDefinition("sd", STORAGE_DESC_TYPE, Multiplicity.REQUIRED, true, null),
-//                        attrDef("owner", DataTypes.STRING_TYPE), attrDef("createTime", DataTypes.LONG_TYPE),
-//                        attrDef("lastAccessTime", DataTypes.LONG_TYPE), attrDef("retention", DataTypes.LONG_TYPE),
-//                        attrDef("viewOriginalText", DataTypes.STRING_TYPE),
-//                        attrDef("viewExpandedText", DataTypes.STRING_TYPE), attrDef("tableType", DataTypes.STRING_TYPE),
-//                        attrDef("temporary", DataTypes.BOOLEAN_TYPE),
-//                        new AttributeDefinition(COLUMNS_ATTRIBUTE, DataTypes.arrayTypeName(COLUMN_TYPE),
-//                                Multiplicity.COLLECTION, true, null));
-//
-//        HierarchicalTypeDefinition<ClassType> loadProcessClsDef = TypesUtil
-//                .createClassTypeDef(LOAD_PROCESS_TYPE, LOAD_PROCESS_TYPE, ImmutableSet.of("Process"),
-//                        attrDef("userName", DataTypes.STRING_TYPE), attrDef("startTime", DataTypes.LONG_TYPE),
-//                        attrDef("endTime", DataTypes.LONG_TYPE),
-//                        attrDef("queryText", DataTypes.STRING_TYPE, Multiplicity.REQUIRED),
-//                        attrDef("queryPlan", DataTypes.STRING_TYPE, Multiplicity.REQUIRED),
-//                        attrDef("queryId", DataTypes.STRING_TYPE, Multiplicity.REQUIRED),
-//                        attrDef("queryGraph", DataTypes.STRING_TYPE, Multiplicity.REQUIRED));
-//
-//        HierarchicalTypeDefinition<ClassType> viewClsDef = TypesUtil
-//                .createClassTypeDef(VIEW_TYPE, VIEW_TYPE, null,
-//                        TypesUtil.createUniqueRequiredAttrDef("name", DataTypes.STRING_TYPE),
-//                        new AttributeDefinition("db", DATABASE_TYPE, Multiplicity.REQUIRED, false, null),
-//                        new AttributeDefinition("inputTables", DataTypes.arrayTypeName(TABLE_TYPE),
-//                                Multiplicity.COLLECTION, false, null));
-//
-//        HierarchicalTypeDefinition<TraitType> dimTraitDef = TypesUtil.createTraitTypeDef("Dimension",  "Dimension Trait", null);
-//
-//        HierarchicalTypeDefinition<TraitType> factTraitDef = TypesUtil.createTraitTypeDef("Fact", "Fact Trait", null);
-//
-//        HierarchicalTypeDefinition<TraitType> piiTraitDef = TypesUtil.createTraitTypeDef("PII", "PII Trait", null);
-//
-//        HierarchicalTypeDefinition<TraitType> metricTraitDef = TypesUtil.createTraitTypeDef("Metric", "Metric Trait", null);
-//
-//        HierarchicalTypeDefinition<TraitType> etlTraitDef = TypesUtil.createTraitTypeDef("ETL", "ETL Trait", null);
-//
-//        HierarchicalTypeDefinition<TraitType> jdbcTraitDef = TypesUtil.createTraitTypeDef("JdbcAccess", "JdbcAccess Trait", null);
-//
-//        HierarchicalTypeDefinition<TraitType> logTraitDef = TypesUtil.createTraitTypeDef("Log Data", "LogData Trait",  null);
-//
-//        return TypesUtil.getTypesDef(ImmutableList.<EnumTypeDefinition>of(), ImmutableList.<StructTypeDefinition>of(),
-//                ImmutableList.of(dimTraitDef, factTraitDef, piiTraitDef, metricTraitDef, etlTraitDef, jdbcTraitDef, logTraitDef),
-//                ImmutableList.of(dbClsDef, storageDescClsDef, columnClsDef, tblClsDef, loadProcessClsDef, viewClsDef));
-//    }
+    /**
+     *
+     * @param navigatorNode
+     * @return
+     */
+    private Referenceable createEntity(NavigatorNode navigatorNode) {
+        String name = navigatorNode.getName();
+        Map<String, String> schema = navigatorNode.getSchema();
+        List<String> inputNodes = navigatorNode.getInputNodes();
+        List<String> outputNodes = navigatorNode.getOutputNodes();
 
-
-    public static void main(String[] args) {
-
+        Referenceable referenceable = new Referenceable(name, "TODO");
+        for(Map.Entry<String, String> schem : schema.entrySet()) {
+            referenceable.set(schem.getKey(), schem.getValue());
+        }
+        referenceable.set("inputs", inputNodes);
+        referenceable.set("outputs", outputNodes);
+        return referenceable;
     }
+
 }
